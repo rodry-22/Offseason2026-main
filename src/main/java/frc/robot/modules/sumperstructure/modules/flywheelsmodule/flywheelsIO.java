f757625d50311d81fe2fb1f5cca07fc6a54a0559
Endermite123 <a01802168@gmail.com>
2026-09-21 16:02:43 -0600

    Ahora si

--- src/main/java/frc/robot/modules/sumperstructure/modules/flywheelsmodule/flywheelsIO.java
+++ src/main/java/frc/robot/modules/sumperstructure/modules/flywheelsmodule/flywheelsIO.java
@@ -1,5 +1,35 @@
 package frc.robot.modules.sumperstructure.modules.flywheelsmodule;
 
-public class flywheelsIO {
+import com.stzteam.features.marsprocessor.Fallback;
+import com.stzteam.features.unitprocessor.Unit;
+import com.stzteam.mars.models.singlemodule.Data;
+import com.stzteam.mars.models.singlemodule.IO;
+import frc.robot.modules.sumperstructure.modules.flywheelsmodule.flywheelsIO.FlyWheelsInputs;
+@Fallback
+public interface flywheelsIO extends IO<FlyWheelsInputs> {
+    
+    public static class FlyWheelsInputs extends Data<FlyWheelsInputs> {
+
+    @Unit(value = "Volts", group = "FlyWheel")
+    public double appliedVolts = 0;
+
+
+
+    @Unit(value = "RPM", group = "FlyWheel")
+    public double targetRPM = 0;
+
+    @Unit(value = "RPM", group = "FlyWheel")
+    public double velocityRPM = 0;
+
+    public double current = 0;
+  }
+
+  public void applyOutput(@Unit(value = "Volts", group = "FlyWheel") double volts);
+
+  public void setSpeed(@Unit(value = "DutyCycle", group = "FlyWheel") double speed);
+
+  public void setTargetRPM(@Unit(value = "RPM", group = "FlyWheel") double rpm);
+
+
     
 }
